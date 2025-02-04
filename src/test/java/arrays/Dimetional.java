ec0ee4df4f7fdba810ff0dc04496d1ea2119b11e
masud3386 <masud3386@yahoo.com>
2025-02-04 18:58:12 -0500

    this is my first commiting java code

---
+++ src/test/java/arrays/Dimetional.java
@@ -0,0 +1,29 @@
+package arrays;
+
+public class Dimetional {
+
+	public static void main(String[] args) {
+
+		int a[][]= {
+				{100,200},
+				{300,400},
+				{500,600},
+				
+		        };
+		
+		System.out.println(a.length);
+		System.out.println(a[1].length);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
